357973fb7b7a9a5374996128c5557e7812355723
Steven Mycynek <Steven Mycynek>
2017-03-06 10:58:53 -0600

    add manifest

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(name='area_under_curve',
-      version='0.3',
+      version='0.4',
       description='Calculate area under curve',
       url='https://github.com/smycynek/area_under_curve',
       author='Steven Mycynek',
       author_email='sv@stevenvictor.net',
       license='MIT',
-      Classifiers=[
+      classifiers=[
           'Development Status :: 3 - Alpha',
           'Intended Audience :: Developers',
           'License :: OSI Approved :: MIT License',
